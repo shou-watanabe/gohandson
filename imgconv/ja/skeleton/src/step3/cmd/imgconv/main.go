b07ef1167f4557d40f3e071f69c36d5fb7dd8aed
shou-watanabe <watanabeshou1520@gmail.com>
2021-11-23 22:16:50 +0900

    3 complete

--- imgconv/ja/skeleton/src/step3/cmd/imgconv/main.go
+++ imgconv/ja/skeleton/src/step3/cmd/imgconv/main.go
@@ -19,17 +19,22 @@ func run() error {
 		return fmt.Errorf("ファイルが開けませんでした。%s", src)
 	}
 	// TODO: 関数終了時にファイルを閉じる
+	defer sf.Close()
 
 	df, err := os.Create(dst)
 	if err != nil {
 		return fmt.Errorf("ファイルを書き出せませんでした。%s", dst)
 	}
 	// TODO: 関数終了時にファイルを閉じる
+	defer df.Close()
 
 	scanner := bufio.NewScanner(sf)
 	// TODO: sfから1行ずつ読み込み、"行数:"を前に付けてdfに書き出す。
-
+	for i := 1; scanner.Scan(); i++ {
+		fmt.Fprintf(df, "%d:%s\n", i, scanner.Text())
+	}
 	// TODO: scannerから得られたエラーを返す
+	return scanner.Err()
 }
 
 func main() {
